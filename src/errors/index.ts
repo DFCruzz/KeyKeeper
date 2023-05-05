30c8578f82f758b1aa39cb89062eb8174ec3b7bb
DFCruzz <diegofcruzz@gmail.com>
2023-05-05 15:04:20 -0300

    feat: errors files and error middleware added

---
+++ src/errors/index.ts
@@ -0,0 +1,5 @@
+export * from "./conflict-error"
+export * from "./forbidden-error"
+export * from "./invalid-email-error"
+export * from "./not-found-error"
+export * from "./unauthorized-error"
---
+++ src/middlewares/error-handling-middleware.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import { ApplicationError } from '@/protocols';
+
+export function handleApplicationErrors(
+  err: ApplicationError | Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) {
+  if (err.name === 'ConflictError' || err.name === 'InvalidEmailError') {
+    return res.status(httpStatus.CONFLICT).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name === 'UnauthorizedError') {
+    return res.status(httpStatus.UNAUTHORIZED).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name === 'NotFoundError') {
+    return res.status(httpStatus.NOT_FOUND).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name === 'ForbiddenError') {
+    return res.status(httpStatus.FORBIDDEN).send({
+      message: err.message,
+    });
+  }
+
+  console.error(err.name);
+  res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+    error: 'InternalServerError',
+    message: 'Internal Server Error',
+  });
+}
