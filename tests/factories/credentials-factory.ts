2765491f2cbd312a4818e05215befbb8ef75bb78
DFCruzz <diegofcruzz@gmail.com>
2023-05-09 10:20:32 -0300

    feat credentials and network facotry added

--- tests/factories/credentials-factory.ts
+++ tests/factories/credentials-factory.ts
@@ -1,1 +1,24 @@
 import { faker } from "@faker-js/faker";
+import { createUser } from "./users-factory";
+import { User, Credential } from "@prisma/client";
+import Cryptr from "cryptr"
+import { prisma } from "@/config";
+
+
+const cryptr = new Cryptr(`${process.env.CRYPTR_KEY}`)
+
+
+export async function createCredential(user?: User): Promise<Credential> {
+    const incomingUser = user || (await createUser());
+    const encryptedPassword = cryptr.encrypt(faker.internet.password(10))
+    
+    return prisma.credential.create({
+        data: {
+          title: faker.lorem.sentence(),
+          url: faker.internet.url(),
+          username: faker.internet.userName(),
+          password: encryptedPassword,
+          userId: incomingUser.id,
+        },
+      });
+}
--- tests/factories/network-factory.ts
+++ tests/factories/network-factory.ts
@@ -1,1 +1,23 @@
 import { faker } from "@faker-js/faker";
+import { createUser } from "./users-factory";
+import { User, Network } from "@prisma/client";
+import Cryptr from "cryptr"
+import { prisma } from "@/config";
+
+
+const cryptr = new Cryptr(`${process.env.CRYPTR_KEY}`)
+
+
+export async function createNetwork(user?: User): Promise<Network> {
+    const incomingUser = user || (await createUser());
+    const encryptedPassword = cryptr.encrypt(faker.internet.password(10))
+    
+    return prisma.network.create({
+        data: {
+          title: faker.lorem.words(),
+          network: faker.lorem.word(),
+          password: encryptedPassword,
+          userId: incomingUser.id,
+        },
+      });
+}
